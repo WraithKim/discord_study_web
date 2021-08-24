4f93ed8d20f220c611a52868a2ba2382f15acacd
Donghwan Kim <wraithtorch@gmail.com>
2021-08-24 14:24:50 +0900

    누락된 SpringExtension 추가

--- src/test/java/com/caurgk/studylog/HealthCheckTest.java
+++ src/test/java/com/caurgk/studylog/HealthCheckTest.java
@@ -1,17 +1,20 @@
 package com.caurgk.studylog;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.json.JsonParser;
 import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HealthCheckTest {
 
